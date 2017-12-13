3f24a8f81cfcbdbb0fb5619f94de08547416517b
René Majewski <rene.majewski@gmx.de>
2017-12-13 17:56:51 +0100

    isArray zu Arguments hinzugefügt.

--- test/arguments/integer_test.rb
+++ test/arguments/integer_test.rb
@@ -3,6 +3,30 @@ require_relative 'arguments_helper'
 module Arguments
   class IntegerTest < Arguments::ArgumentsHelper
     def setup()
+      @test = testInteger
+      @arguments = ::ReneMajewski::RubyChecks::Arguments.new(@test)
+    end
+
+    def test_response_true_with_integer_as_object()
+      assert_equal true, @arguments.isInteger()
+      assert_equal "", @arguments.message
+    end
+
+    def test_raise_argument_error_with_no_integer_as_object()
+      @test = testString
+      @arguments = ::ReneMajewski::RubyChecks::Arguments.new(@test)
+
+      exception = assert_raises(ArgumentError) {@arguments.isInteger()}
+      assert_equal ::ReneMajewski::RubyChecks::StandardMessages.messageNoInteger(), exception.message
+      assert_equal ::ReneMajewski::RubyChecks::StandardMessages.messageNoInteger(), @arguments.message
+    end
+
+    def test_false_with_no_integer_as_object_and_no_raised_error()
+      @test = testArray
+      @arguments = ::ReneMajewski::RubyChecks::Arguments.new(@test, false)
+
+      assert_equal false, @arguments.isInteger()
+      assert_equal ::ReneMajewski::RubyChecks::StandardMessages.messageNoInteger(), @arguments.message
     end
   end
 end
